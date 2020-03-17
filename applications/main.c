629c709fa893ea619de62d8ec0c1cb6c1136e3d4
wangxiufeng <1017845778@qq.com>
2020-03-17 23:43:26 +0800

    第一次提交

---
+++ applications/main.c
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2006-2019, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2019-09-09     RT-Thread    first version
+ */
+
+#include <rtthread.h>
+#include <board.h>
+#include <rtdevice.h>
+
+#define DBG_TAG "main"
+#define DBG_LVL DBG_LOG
+#include <rtdbg.h>
+
+/* PLEASE DEFINE the LED0 pin for your board, such as: PA5 */
+#define LED0_PIN    GET_PIN(E, 7)
+
+int main(void)
+{
+    int count = 1;
+    /* set LED0 pin mode to output */
+    rt_pin_mode(LED0_PIN, PIN_MODE_OUTPUT);
+
+    while (count++)
+    {
+        rt_pin_write(LED0_PIN, PIN_HIGH);
+        rt_thread_mdelay(500);
+        rt_pin_write(LED0_PIN, PIN_LOW);
+        rt_thread_mdelay(500);
+    }
+
+    return RT_EOK;
+}
